9a12ab812ac097492b3da3ddecd7155598af1eca
Setty32 <xkaisl00@stud.fit.vutbr.cz>
2015-04-12 13:32:05 +0200

    Vytvoreny zakladni tridy

---
+++ synthesize.cpp
@@ -0,0 +1,5 @@
+#include "synthesize.h"
+
+synthesize::synthesize()
+{
+}
---
+++ synthesize.h
@@ -0,0 +1,10 @@
+#ifndef SYNTHESIZE_H
+#define SYNTHESIZE_H
+
+class synthesize
+{
+public:
+    synthesize();
+};
+
+#endif // SYNTHESIZE_H
