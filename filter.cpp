bf3b9e5e86a5fbaa3eccb5304b0655e3c9d95b99
Setty32 <xkaisl00@stud.fit.vutbr.cz>
2015-04-12 14:13:20 +0200

    prvni vydani filtru

--- filter.cpp
+++ filter.cpp
@@ -3,3 +3,47 @@
 filter::filter()
 {
 }
+
+void filter::loadInputData(std::vector<double> *b1, std::vector<double> *a1, std::vector<double> *X1, std::vector<double> *z1){
+    this->b = b1;
+    this->a = a1;
+    this->input = X1;
+    this->zi = z1;
+}
+
+void filter::getFirstOutput(){
+    return &output1;
+}
+
+void filter::getSecondOutput(){
+    return &output2;
+}
+
+void filter::doFilter(){
+    unsigned n = a->size();
+    if(zi->size() != n){
+        zi->resize(n);
+    }
+
+    zi->at(n-1) = 0;
+
+    for (unsigned i = 0; i < b->size(); i++) {
+        b->at(i) = b->at(i) / b->at(0);
+        a->at(i) = a->at(i) / a->at(0);
+    }
+
+    output1.clear();
+    output1.resize(input->size());
+    for(unsigned i = 0; i < input->size(); i++){
+        output1.at(i) = b->at(0) * input->at(i) + zi->at(0);
+        for (unsigned j = 1; j < n; j++) {
+            zi->at(j - 1) = b->at(j) * input->at(i) + zi->at(j) - a->at(j) * output1.at(i);
+        }
+    }
+
+    output2.clear();
+    output2.resize(zi->size() - 1);
+    for (unsigned i = 0; i < zi->size() - 1; i++) {
+        output2.at(i) = zi->at(i);
+    }
+}
--- filter.h
+++ filter.h
@@ -1,10 +1,41 @@
 #ifndef FILTER_H
 #define FILTER_H
 
+#include <vector>
+
 class filter
 {
+private:
+    std::vector<double> *b; //Numeral coefficients of rational transfer func.
+    std::vector<double> *a; //Determinator coefficients of rational transfer func.
+    std::vector<double> *input; //X input vector
+    std::vector<double> *zi; //Initial conditions
+
+    std::vector<double> output1;
+    std::vector<double> output2;
 public:
+
+
     filter();
+    void loadInputData(std::vector<double> *b1, std::vector<double> *a1, std::vector<double> *X1, std::vector<double> *z1);
+    void doFilter();
+    std::vector<double> *getFirstOutput();
+    std::vector<double> *getSecondOutput();
 };
 
 #endif // FILTER_H
+
+//function [Y, z] = myFilter(b, a, X, z)
+//% Author: Jan Simon, Heidelberg, (C) 2011
+//n    = length(a);
+//z(n) = 0;      % Creates zeros if input z is omitted
+//b = b / a(1);  % [Edited, Jan, 26-Oct-2014, normalize parameters]
+//a = a / a(1);
+//Y    = zeros(size(X));
+//for m = 1:length(Y)
+//   Y(m) = b(1) * X(m) + z(1);
+//   for i = 2:n
+//      z(i - 1) = b(i) * X(m) + z(i) - a(i) * Y(m);
+//   end
+//end
+//z = z(1:n - 1);
